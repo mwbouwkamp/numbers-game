af08c1078c67ef74e2eb9931be1c37dd051bfb54
mwbouwkamp <mwbouwkamp@gmail.com>
2018-11-23 15:55:00 +0100

    Add LayoutInerface

--- app/src/main/java/nl/limakajo/numbers/layouts/BasicLayout.java
+++ app/src/main/java/nl/limakajo/numbers/layouts/BasicLayout.java
@@ -11,7 +11,7 @@ import nl.limakajo.numbers.gameObjects.TextBox;
 import nl.limakajo.numbers.main.MainActivity;
 import nl.limakajo.numbers.utils.Attributes;
 
-public class BasicLayout {
+public class BasicLayout implements layoutInterface {
 
     protected EnumMap<LayoutElementsKeys, LayoutObject> layoutObjects = new EnumMap<LayoutElementsKeys, LayoutObject>(LayoutElementsKeys.class);
 
---
+++ app/src/main/java/nl/limakajo/numbers/layouts/layoutInterface.java
@@ -0,0 +1,7 @@
+package nl.limakajo.numbers.layouts;
+
+import android.graphics.Canvas;
+
+public interface layoutInterface {
+    public void draw(Canvas canvas);
+}
