f08445a09fa66a2e7d398febaa601332a58a9129
mwbouwkamp <mwbouwkamp@gmail.com>
2018-12-02 18:31:00 +0100

    Solve TODOs DatabaseUtils.class

--- app/src/main/java/nl/limakajo/numbers/layouts/GameOverLayout.java
+++ app/src/main/java/nl/limakajo/numbers/layouts/GameOverLayout.java
@@ -1,7 +1,5 @@
 package nl.limakajo.numbers.layouts;
 
-import android.graphics.Rect;
-
 import nl.limakajo.numbers.gameObjects.LayoutObject;
 import nl.limakajo.numbers.gameObjects.TextBox;
 import nl.limakajo.numbers.utils.Attributes;
@@ -22,7 +20,7 @@ public class GameOverLayout extends LogoWithAreaForTextLayout {
                 "Out of time",
                 Attributes.TextAllignment.XYCENTERED,
                 layoutObjects.get(LayoutElementsKeys.MAINTEXT_AREA).getArea(),
-                Attributes.TEXTBOX_LARGE_PAINT);
+                Attributes.TEXTBOX_NORMAL_PAINT);
         layoutObjects.put(LayoutElementsKeys.GAMEOVER_TEXT, gameOverText);
     }
 }
