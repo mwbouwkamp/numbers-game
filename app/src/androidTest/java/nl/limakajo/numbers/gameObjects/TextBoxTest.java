c7e24d8fe16a2a110774795614abda69daf10b2f
mwbouwkamp <mwbouwkamp@gmail.com>
2018-12-16 11:29:23 +0100

    Update test

--- app/src/androidTest/java/nl/limakajo/numbers/gameObjects/TextBoxTest.java
+++ app/src/androidTest/java/nl/limakajo/numbers/gameObjects/TextBoxTest.java
@@ -1,5 +1,6 @@
 package nl.limakajo.numbers.gameObjects;
 
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
 
@@ -14,17 +15,30 @@ import static org.junit.Assert.*;
 
 public class TextBoxTest {
 
+
     @Test
     public void createTextBoxAndGetAttributesTest() {
-        String text = "test";
-        Attributes.TextAllignment textAllignment = Attributes.TextAllignment.XYCENTERED;
+        String text = "text";
         Rect rect = new Rect(0,0,100,100);
         Paint paint = new Paint();
-        TextBox textBox = new TextBox(text, textAllignment, rect, paint);
+        paint.setColor(Color.RED);
+        TextBox textBox = new TextBox(text, Attributes.TextAllignment.XYCENTERED, rect, paint);
         Assert.assertEquals("Retrieve the right text", "text", textBox.getText());
         Assert.assertEquals("Retrieve the right allignment", Attributes.TextAllignment.XYCENTERED, textBox.getAlignment());
         Assert.assertEquals("Retrieve the right Rect", rect, textBox.getArea());
         Assert.assertEquals("Retrieve a Paint with the right attributes", 0, new PaintComparator().compare(paint, textBox.getPaint()));
+    }
+
+    @Test
+    public void createTextBoxSetText() {
+        String textBefore = "textbefore";
+        String textAfter = "textafter";
+        Rect rect = new Rect(0,0,100,100);
+        Paint paint = new Paint();
+        paint.setColor(Color.RED);
+        TextBox textBox = new TextBox(textBefore, Attributes.TextAllignment.XYCENTERED, rect, paint);
+        textBox.setText(textAfter);
+        Assert.assertEquals("Retrieve the right text", "textafter", textBox.getText());
     }
 
 }
